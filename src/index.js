52ddf17fc95eb83b3053384d39c72f42c24f5035
Fer Hernandez <fh.1111@gmail.com>
2019-03-02 16:59:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -28,5 +28,6 @@ Order.defaultProps = {
 Order.propTypes = {
   cone: PropTypes.bool
   size: PropTypes.string
-  scoops: PropTypes.array.isRequired
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired
+  orderInfo: PropTypes.object.isRequired
 };
